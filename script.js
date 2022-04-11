d2cbed7c157019a9903bc261d61e42c9611453f7
Bella <98971465+Bellawareness@users.noreply.github.com>
2022-04-11 23:20:08 +0000

    added about, will add to all published projects

--- script.js
+++ script.js
@@ -97,3 +97,37 @@ var bill = Number(prompt("¿Cual es tu total? - What is your total? "));
     }
 })();
 
+
+
+
+//created an object with list of attributes 
+const Iam = {
+  Name: "Bella",
+  Skills : "CSS, HTML, Javascript and Git/Github.",
+
+  //a function to call list and input info needed for homework 
+  //considering translating in spanish 
+  myPortfoilo : function() {
+    return "Hi, it's Bella I am enhancing my skills by participating in Google's UX Designer Certificate course. I recently learned " + this.Skills + " You may see my potfolio at the link below" ; 
+  }
+};
+
+
+//print to console t
+//console.log(Iam.myPortfoilo());
+
+
+
+//code to take log answers to html in a typewriter effect
+
+var i = 0;
+var txt = Iam.myPortfoilo();
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("thediv").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
