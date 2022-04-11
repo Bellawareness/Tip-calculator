f0dc89ab33c868ea60ecd2eb2928c7dc9bd3e04d
Bella <98971465+Bellawareness@users.noreply.github.com>
2022-04-11 02:51:35 +0000

    updated background

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //How the idea started I wanted a simplier functional tip website. 
+//possible ideas currency and answer in spanish, round to decimal 2
 // const tipCalculator = (quality, total) => {
 // 	  var text;
 //   var quality = prompt("How was this service? ");
@@ -21,12 +22,15 @@
 let element = document.querySelector(".excellent");
 
 function excellent (){
-	var total = prompt("What is your total? ");
+	var bill = Number(prompt(" ¿Cual es tu total? - What is your total? "));
 	//element.style.backgroundColor = "black";
   //element.style.color = "pink";
   element.innerHTML = "Tip 30% "; 
-              
-console.log('They went above and beyond for you and they deserve it, tip them : $'+ Math.round((total)* .30));
+    let amount = (bill * .30).toFixed(2); 
+	 let totalbill = (+bill + +amount);
+
+														 
+console.log('Hicieron todo lo posible por ti y se lo merecen, dales una propina : $'+ amount + ' Tu total es: $' +  totalbill);
 
 }
 //element.onclick = excellent;
@@ -35,13 +39,15 @@ console.log('They went above and beyond for you and they deserve it, tip them : 
 
  let element1 = document.querySelector(".great");
 
- function great (total){
- 		  var total = prompt("What is your total? ");
- //	element1.style.backgroundColor = "black";
-  // element1.style.color = "green";
+ function great (){
+	var bill = Number(prompt("¿Cual es tu total? - What is your total? "));
+	    let amount = (bill * .20).toFixed(2); 
+	 let totalbill = (+bill + +amount);
+
   element1.innerHTML = "Tip 20% "; 
 	 
-	 console.log('They tried  for you and they deserve it, tip them : $'+ Math.round((total)* .20));
+	 
+	 console.log('Lo intentaron por ti y se lo merecen, dales propina: $'+ tip + ' Tu total es $:' +  totalbill);
  }
 // element1.onclick = veryGood;
  //console.log(veryGood());
@@ -49,25 +55,29 @@ console.log('They went above and beyond for you and they deserve it, tip them : 
 
  let element2 = document.querySelector(".good");
 
- function good (total){
- 		  var total = prompt("What is your total? ");
- //	element1.style.backgroundColor = "black";
-  // element1.style.color = "green";
+ function good (){
+var bill = Number(prompt("¿Cual es tu total? - What is your total? "));
+	   let amount = (bill * .15).toFixed(2); 
+	 let totalbill = (+bill + +amount);
   element2.innerHTML = "Tip 15% "; 
 	 
-	 console.log('They did a good job and they deserve it, tip them : $'+ Math.round((total)* .15));
+	 
+	 console.log('Hicieron un buen trabajo y se lo merecen, dales propina: $'+ amount + ' Tu total es: $' +  (totalbill));
  }
+// element1.onclick = veryGood;
+ //console.log(veryGood());
 
 
  let element3 = document.querySelector(".bad");
 
- function bad (total){
- 		  var total = prompt("What is your total? ");
- //	element1.style.backgroundColor = "black";
-  // element1.style.color = "green";
-  element3.innerHTML = "Tip 10% "; 
+ function bad (){
+	 var bill = Number(prompt("¿Cual es tu total? - What is your total? "));
+   let amount = (bill * .05).toFixed(2); 
+	 let totalbill = (+bill + +amount);
+  element3.innerHTML = "Tip 5% "; 
 	 
-	 console.log('They did a terrible job and barely deserve it, tip them : $'+ Math.round((total)* .10));
+	 
+	 console.log('Hicieron un trabajo terrible y apenas se lo merecen, déles una propina:$'+ amount + ' Tu total es: $' +  (totalbill));
  }
 
 
@@ -86,3 +96,4 @@ console.log('They went above and beyond for you and they deserve it, tip them : 
       }
     }
 })();
+
